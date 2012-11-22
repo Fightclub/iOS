fead2b2123a521d0b3d51fd69ef59c1c049ebd37
Peter Tsoi <petertsoi@gmail.com>
2012-11-22 14:45:53 -0800

    Add FCImage class

---
+++ Givair/src/model/FCImage.h
@@ -0,0 +1,15 @@
+//
+//  FCImage.h
+//  Givair
+//
+//  Created by Peter Tsoi on 11/22/12.
+//  Copyright (c) 2012 Fightclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCImage : UIImage {
+    NSURL * mRemotePath;
+}
+
+@end
---
+++ Givair/src/model/FCImage.m
@@ -0,0 +1,13 @@
+//
+//  FCImage.m
+//  Givair
+//
+//  Created by Peter Tsoi on 11/22/12.
+//  Copyright (c) 2012 Fightclub. All rights reserved.
+//
+
+#import "FCImage.h"
+
+@implementation FCImage
+
+@end
